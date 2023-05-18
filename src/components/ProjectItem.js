d6cd59966af2e776d4929fbed90b0e24b4068e54
Kyuhee Lee <kyuhee.lee.1011@gmail.com>
2023-05-17 21:37:11 -0400

    list and keys lab done

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -6,6 +6,9 @@ function ProjectItem({ name, about, technologies }) {
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
+        {technologies.map((techology) => {
+          return <span key={techology}> {techology}</span>;
+        })}
         {/* render a <span> for each technology in the technologies array */}
       </div>
     </div>
